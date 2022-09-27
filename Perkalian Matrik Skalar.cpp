4213022893a58d869c1f764a68851ae0259d96ad
Bariq Adyatma <53467656+bariqadyatam@users.noreply.github.com>
2022-09-27 21:56:02 +0700

    Add files via upload

---
+++ Perkalian Matrik Skalar.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+	int i, j, m, n, skalar, matriksA[10][10], hasil[10][10];
+  
+	cout << " --PROGRAM PERKALIAN SKALAR MATRIKS --" << endl;
+	cout << "=====================================" << endl;
+	cout << " Jumlah baris matriks : "; cin >> m;
+	cout << " Jumlah kolom matriks : "; cin >> n;
+	
+	cout << endl;
+	cout << " Masukkan Nilai Skalar : "; cin >> skalar;
+  	cout << " Matrix A : \n";
+	for(i=0; i<m; i++){
+		for(j=0; j<n; j++){
+      		cin>>matriksA[i][j];
+    	}
+  	}
+  	cout<<" Hasil perkalian skalar matrix : \n";
+  	for(i=0; i<m; i++){
+    	for(j=0; j<n; j++){
+      		hasil[i][j]=skalar*matriksA[i][j];
+      		cout<<hasil[i][j]<<"\t";
+    	}
+    cout<<endl;
+	}
+  
+	getch();
+	return 0;
+}
